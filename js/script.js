ce1780175c4b86c350a4d060190e1d970abd2d1a
Jumelis Rosales <jumelisrosales@gmail.com>
2023-06-04 22:30:42 +0200

    se agrega login vinculado con funcionalidades

--- js/script.js
+++ js/script.js
@@ -48,13 +48,23 @@ addSoundEvent(tom2_map);
 addSoundEvent(tom3_map);
 addSoundEvent(tom4_map);
 
-function check_user() {
-  const params = new URLSearchParams(window.location.search);
-  const user_name = params.get('username');
-  if (user_name == '') {
-    alert('Usuario no logeado');
-  } else {
-    alert('Bienvenido ' + user_name + '!!');
+function checkUser() {
+  // obtener la url completa del navegador
+  let urlWithParamUserName = window.location.search; 
+  // saca de la url todos los parametros con sus respectivos valores
+  // URLSearchParams ignora todo lo que está detras del signo de interrogación (?)
+  let params = new URLSearchParams(urlWithParamUserName); 
+  // obtiene el valor del parametro param-name que llega en la url 
+  let userName = params.get('param-name');
+
+  if (userName != null) { // si userName es distinto de null se ejecuta
+        // ocultamos el item Login
+        document.getElementById("loginItem").style.display = "none";// etiqueta <a>
+        // obtenemos el item activeUser
+        let navItemActiveUser = document.getElementById("activeUser"); // etiqueta <a>
+        // incorpora el texto (nombre del usuario) al item activeUser
+        navItemActiveUser.textContent = userName;
+        // muestro el item activeUser
+        navItemActiveUser.style.display = "block";
   }
 }
-
--- src/login/login.js
+++ src/login/login.js
@@ -1,4 +1,9 @@
-function send_form() {
-  user_name = document.getElementById("typeUserX").value;
-  window.location.href = "../../index.html?username=" + user_name;
+function sendForm() {
+  let userName = document.getElementById("typeUserX").value;
+  window.location.href = "../../index.html?param-name=" + userName;
+}
+
+
+function redirectHome() {
+ window.location.href = "../../index.html";
 }
