74ae7141e655671d4ded1ac2eebf4b1b3144a3eb
Jumelis Rosales <jumelisrosales@gmail.com>
2023-06-05 12:10:31 +0200

    se eliminan los comentarios

--- js/login.js
+++ js/login.js
@@ -1,17 +1,12 @@
 function sendForm() {
-  //toma la informacion que fue agregada por el usuario en el imput username desde el HTML y lo guarda en la variable userName
   let userName = document.getElementById("typeUserX").value;
-  //la propiedad window.location.href se usa para redireccionar la pagina a una url construida para simular el flujo normal de una web
   if (userName.length > 0) {
-    //adicional a esto le agrega al string param-name que sera igual a la variable nombre de usuario
     window.location.href = "../index.html?param-name=" + userName;
   } else {
   alert ("Por Favor llene los campos correspondientes")
 }
 }
-// cuando se hace click sobre la imagen con la propiedad window.location.href
-//se redirecciona la pagina web al index.html (pagina principal)
+
 function redirectHome() {
-  //los ../ son para salir de un directorio, cada ../ es un nivel
  window.location.href = "../index.html";
 }
--- js/script.js
+++ js/script.js
@@ -12,22 +12,14 @@ document.addEventListener('keypress',(e)=>{
 });
 
 function checkUser() {
-        // obtener la url completa del navegador
         let urlWithParamUserName = window.location.search; 
-        // saca de la url todos los parametros con sus respectivos valores
-        // URLSearchParams ignora todo lo que está detras del signo de interrogación (?)
         let params = new URLSearchParams(urlWithParamUserName); 
-        // obtiene el valor del parametro param-name que llega en la url 
         let userName = params.get('param-name');
       
-        if (userName != null) { // si userName es distinto de null se ejecuta
-              // ocultamos el item Login
-              document.getElementById("loginItem").style.display = "none";// etiqueta <a>
-              // obtenemos el item activeUser
-              let navItemActiveUser = document.getElementById("activeUser"); // etiqueta <a>
-              // incorpora el texto (nombre del usuario) al item activeUser
+        if (userName != null) { 
+              document.getElementById("loginItem").style.display = "none";
+              let navItemActiveUser = document.getElementById("activeUser"); 
               navItemActiveUser.textContent = userName;
-              // muestro el item activeUser
               navItemActiveUser.style.display = "block";
         }
       }
