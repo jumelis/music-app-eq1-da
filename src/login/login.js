5e823cbf7bfaf2a515ffd09aa1d691b56b124150
Jumelis Rosales <jumelisrosales@gmail.com>
2023-06-05 11:55:17 +0200

    se reorganiza la estructura del proyecto y se agrega else con alert en la funcion sendForm().

--- src/login/login.js
+++
@@ -1,9 +0,0 @@
-function sendForm() {
-  let userName = document.getElementById("typeUserX").value;
-  window.location.href = "../../index.html?param-name=" + userName;
-}
-
-
-function redirectHome() {
- window.location.href = "../../index.html";
-}
